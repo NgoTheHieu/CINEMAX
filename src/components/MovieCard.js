e678dd7ca2962c85994466c298a7a5dfa141aa01
NgoTheHieu <60764766+NgoTheHieu@users.noreply.github.com>
2020-06-01 16:07:44 +0700

    Add files via upload

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,11 @@
   
 import React from "react";
-import { Card, ListGroupItem, ListGroup } from "react-bootstrap";
+import { Card, ListGroupItem, ListGroup,Button } from "react-bootstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 export default function MovieCard(props) {
   let movie = props.movie;
+  let openModal = props.showModal;
   let language = props.movie.original_language;
   let language3;
   const language2 = () => {
@@ -21,8 +23,8 @@ export default function MovieCard(props) {
     <Card className="Card m-5 row center " style={{ width: "18rem" }}>
       <Card.Img
         variant="top" href=" "
-        src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`
-        onClick={}}
+        src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`}
+      onClick={()=> openModal}
       />
       <Card.Body>
         <Card.Title>{movie.original_title}</Card.Title>
@@ -38,8 +40,12 @@ export default function MovieCard(props) {
     <ListGroupItem className="text-center">Rating: {movie.vote_average} </ListGroupItem>
       </ListGroup>
       <Card.Body>
-        <Card.Link href="#">IMDB</Card.Link>
-        <Card.Link href="#">Watch Trailer</Card.Link>
+      <Button class="d-flex justify-content-center banner" id="load-more-btn" variant="info" type="button" >
+      
+        <Card.Link
+      onClick={()=> openModal(movie.id)}>Trailer</Card.Link>
+        </Button>
+        <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
   );
